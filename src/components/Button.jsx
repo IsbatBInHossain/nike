997fc2e14fba45c0fa7b7cf531c720561be5e27b
Isbat-Bin-Hossain <isbat31416@gmail.com>
2023-08-28 17:23:52 +0600

    Complete special offers section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,18 @@
-const Button = ({ label, iconUrl }) => {
+const Button = ({
+  label,
+  iconUrl,
+  backgroundColor,
+  borderColor,
+  textColor,
+}) => {
   return (
-    <button className='flex justify-center items-center gap-2 px-7 py-5 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'>
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-5 border font-montserrat text-lg leading-none ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : 'bg-coral-red text-white border-coral-red'
+      } rounded-full`}
+    >
       {label}
       {iconUrl && (
         <img src={iconUrl} alt='icon' className='ml-2 rounded-full w-5 h-5' />
